8f302d35ad7b3e626a1d18610ec492ed84f12b8d
David Nemeth Csoka <encse@csokavar.hu>
2017-04-30 08:17:30 +0200

    q

--- packages/moduleA/webpack.config.js
+++ packages/moduleA/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     output: {
         filename: "[name].js",
-        path: __dirname + "/dist/",
+        path: __dirname + "/build/",
         libraryTarget: "umd"
 
     },
